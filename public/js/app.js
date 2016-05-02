0b111c66f1a1ca9a2bdca461568bcdd779486404
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-05-02 22:35:30 +0100

    Add progress bar for XHR requests

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,14 @@ angular.module('openDataHubApp', [
   'md.data.table',
   'sticky',
   'ui.utils.masks',
-  'chart.js'
+  'chart.js',
+  'angular-loading-bar',
 ])
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
       .primaryPalette('teal')
       .accentPalette('green');
-  });
+  })
+  .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = false;
+  }]);

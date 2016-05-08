0b314f78256a4278da1726a156a285f5059e67fd
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-05-08 18:24:03 +0100

    Remove duplicated config
    The configuration for the "test" environment was duplicated

--- config/config.js
+++ config/config.js
@@ -53,31 +53,6 @@ var config = {
         pass: 'rg1014'
     }
   },
-  test: {
-    root: rootPath,
-    encryption_key: 'Fasdf354Dfva5009f',
-    web: {
-    	site_url: 'http://aveiro.m-iti.org',
-    	port:     3000
-    },
-    db: {
-    	host:     'mongodb://'+host,
-    	port:     27017,
-    	database: 'sinais',
-    	user:     '',
-    	password: ''
-    },
-    fs: {
-    	storage_folder: './file_storage/' // main path for file storage
-    },
-    rows_per_page: 50,
-    mailer: {
-  	   host: 'imap.m-iti.org',
-      port: 587,
-      user: 'rodolfo.goncalves@m-iti.org',
-      pass: 'rg1014'
-    }
-  },
   production: {
     root: rootPath,
     encryption_key: 'Fasdf354Dfva5009f',
